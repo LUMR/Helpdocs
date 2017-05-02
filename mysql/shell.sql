23622760649630ff7cfa8768636dc162f46bb61e
lumr <freedomyes@sina.com>
2017-05-02 16:01:18 +0800

    Mysql

--- mysql/shell.sql
+++ mysql/shell.sql
@@ -1,5 +1,12 @@
 -- 运行数据库的SQL脚本
-mysql -u lumr -p <create_database.sql
+mysql -u lumr -p <create_database.sql  
+mysql -u lumr -p -default-character-set=utf8 database1 <create_database.mysql
+
+--修改用户密码
+alter user 'root'@'localhost' identified by 'abcdefg';
+
+--刷新权限
+flush privileges;
 
 --查看数据库字符集
 show variables like 'collation_%';
